51a23f18ef30051862084422100bec435c4e2bd3
Gintautas Miselis <gintautas@miselis.lt>
2016-03-08 21:45:20 +0000

    Test for Codeception issue #2592

---
+++ GetMethodWithQueryStringAndParamsCept.php
@@ -0,0 +1,14 @@
+<?php 
+$I = new FunctionalTester($scenario);
+$I->wantTo('make GET request with query string');
+$I->sendGET('/rest?param1=value1', ['param2' => 'value2']);
+$I->seeResponseIsJson();
+
+$expectedResponse = [
+    'requestMethod' => 'GET',
+    'queryParams' => [
+        'param1' => 'value1',
+        'param2' => 'value2',
+    ],
+];
+$I->seeResponseContainsJson($expectedResponse);
